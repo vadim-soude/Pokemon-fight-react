a0e2b64e6252e0707cc909a95288c37ed028d406
vadimsoude <vadimsoude@gmail.com>
2024-01-29 19:47:34 +0100

    Front

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import './App.css'; // Import the CSS file for styling
+import './PokeFont.ttf';
 
 const PokemonBattle = () => {
-    const [pokemon1HP, setPokemon1HP] = useState(100);
-    const [pokemon2HP, setPokemon2HP] = useState(100);
+
+    const [pokemon1HP, setPokemon1HP] = useState(25);
+    const [pokemon2HP, setPokemon2HP] = useState(17);
 
     const calculateBarColor = (percentage) => {
         // Calculate the color based on the percentage using a gradient from green to red
@@ -15,40 +17,58 @@ const PokemonBattle = () => {
     // https://pokeapi.co
 
     return (
-        <div className="centered-square">
-            <div className="battle-container">
-                <div className="pokemon-container pokemon1">
+        <div>
+            <div className="centered-square">
+                <div className="battle-container">
+                    <div className="pokemon-container pokemon1">
+                        <div className="info-card">
+                            <span className="pokemon-name">
+                                <p>Ditto</p>
+                                <p className="level">Lv3</p>
+                            </span>
+                            <div className="hp-bar"
+                                 style={{
+                                     width: `${pokemon2HP * 100 / 17}%`,
+                                     backgroundColor: calculateBarColor(pokemon2HP * 100 / 17)
+                                 }}></div>
+                        </div>
+                        {/*@Todo S'occuper la transparance de mort*/}
+                        <img className="pokemon-sprite"
+                             alt={"sprite"}
+                             src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"}
+                             width="200" height="200"
+                        />
 
-                    <div className="info-card">
-                        <span className="pokemon-name">Ditto</span>
-                        <span>Lvl : 3</span>
-                        <div className="hp-bar"
-                             style={{width: `${pokemon1HP}%`, backgroundColor: calculateBarColor(pokemon1HP)}}></div>
-                    </div>
-                    {/*@Todo S'occuper la transparance de mort*/}
-                    <img className="pokemon-sprite"
-                         alt={"sprite"}
-                         src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"}
-                         width="300" height="300"
-                    />
-
-                </div>
-
-                <div className="pokemon-container pokemon2">
-                    {/*@Todo S'occuper la transparance de mort*/}
-                    <img className="pokemon-sprite"
-                         alt={"sprite"}
-                         src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png"}
-                         width="300" height="300"
-                    />
-                    <div className="info-card">
-                        <span className="pokemon-name">Pikachu</span>
-                        <span>Lvl : 5</span>
-                        <div className="hp-bar"
-                             style={{width: `${pokemon1HP}%`, backgroundColor: calculateBarColor(pokemon1HP)}}></div>
                     </div>
 
+                    <div className="pokemon-container pokemon2">
+                        {/*@Todo S'occuper la transparance de mort*/}
+                        <img className="pokemon-sprite"
+                             alt={"sprite"}
+                             src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png"}
+                             width="200" height="200"
+                        />
+                        <div className="info-card">
+                        <span className="pokemon-name">
+                            <p>Pikachu</p>
+                            <p className="level">Lv5</p>
+                        </span>
+                            <div className="hp-bar"
+                                 style={{
+                                     width: `${pokemon1HP * 100 / 25}X%`,
+                                     backgroundColor: calculateBarColor(pokemon1HP * 100 / 25)
+                                 }}>
+                            </div>
+                            <span className="pokemon-name">
+                            HP : {pokemon1HP} / 25
+                            </span>
+                        </div>
+                    </div>
                 </div>
+            </div>
+
+            <div className="move-box">
+
             </div>
         </div>
     );
